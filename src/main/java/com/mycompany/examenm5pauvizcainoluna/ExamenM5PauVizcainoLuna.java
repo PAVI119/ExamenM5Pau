1cd1b472a6f28ab9edd672cad04a71a0e74123db
PAVI119 <123946240+PAVI119@users.noreply.github.com>
2023-05-12 10:43:28 +0200

    Add files via upload

---
+++ src/main/java/com/mycompany/examenm5pauvizcainoluna/ExamenM5PauVizcainoLuna.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.examenm5pauvizcainoluna;
+
+/**
+ *
+ * @author PauVizcaino
+ */
+public class ExamenM5PauVizcainoLuna {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
